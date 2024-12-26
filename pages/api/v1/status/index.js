88f1afa8ec42ff4c986a3f51935002f57f462ae3
Felipe Ferreira <contosdeferreira@hotmail.com>
2024-12-26 14:51:57 +0000

    adiciona database.js e .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(request, response) {
-  response.status(200).send({ chave: "média" })
+import database from "../../../../infra/database.js"
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1+1 as sum")
+  console.log(result.rows)
+  response.status(200).send({ chave: result })
 }
