789ae64cef56eff91f35575931146415a4312470
pedigru3 <contosdeferreira@hotmail.com>
2024-12-27 14:42:39 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,33 @@
 import database from "infra/database.js"
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum")
-  console.log(result.rows)
-  response.status(200).send({ chave: result.rows[0].sum })
+  const updatedAt = new Date().toISOString()
+  const databaseVersionResult = await database.query(`
+    SHOW server_version;
+  `)
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version
+
+  const maxConnections = await database.query(`
+    SELECT current_setting('max_connections');
+  `)
+
+  const databaseName = process.env.POSTGRES_DB
+
+  const activeConnections = await database.query({
+    text: `
+      SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;
+    `,
+    values: [databaseName],
+  })
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: Number(maxConnections.rows[0].current_setting),
+        opened_connections: activeConnections.rows[0].count,
+      },
+    },
+  })
 }
--- tests/api/integration/status.test.js
+++
@@ -1,3 +0,0 @@
-test("Testa status", () => {
-  expect(2).toBe(2)
-})
