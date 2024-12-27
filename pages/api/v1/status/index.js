9cf1b262cbcf4ef783d663514f2cce8b31697d01
pedigru3 <contosdeferreira@hotmail.com>
2024-12-27 12:38:13 -0300

    add jsconfig.json

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,7 @@
-import database from "../../../../infra/database.js"
+import database from "infra/database.js"
 
 export default async function status(request, response) {
   const result = await database.query("SELECT 1+1 as sum")
   console.log(result.rows)
-  response.status(200).send({ chave: result })
+  response.status(200).send({ chave: result.rows[0].sum })
 }
