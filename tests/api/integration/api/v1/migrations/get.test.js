6b16adbbc9b137fa09dd9b5a42cf9758eed6fd47
pedigru3 <contosdeferreira@hotmail.com>
2024-12-31 17:06:53 -0300

    adds /api/v1/migrations endpoint

---
+++ tests/api/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,17 @@
+import database from "infra/database.js"
+
+async function clearDatabase() {
+  await database.query("drop schema public cascade; create schema public;")
+}
+
+beforeAll(clearDatabase)
+
+test("Get to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations")
+  const responseBody = await response.json()
+
+  expect(process.env.POSTGRES_HOST).toBe("localhost")
+
+  expect(Array.isArray(responseBody)).toBe(true)
+  expect(responseBody.length).toBeGreaterThan(0)
+})
