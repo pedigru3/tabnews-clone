0c4af3146cdd5b6f1565603a93ce51cf9915e4d9
verbuscomunicacao <ferreira@verbuscomunicacao.com.br>
2025-09-20 07:10:27 -0300

    test: rename all descriptions to use new pattern

--- tests/api/integration/api/v1/status/get.test.js
+++ tests/api/integration/api/v1/status/get.test.js
@@ -4,22 +4,26 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices()
 })
 
-test("Get to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
-  const responseBody = await response.json()
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status")
+      const responseBody = await response.json()
 
-  expect(response.status).toBe(200)
-  expect(responseBody).toEqual({
-    updated_at: expect.any(String),
-    dependencies: {
-      database: {
-        version: "16.0",
-        max_connections: 100,
-        opened_connections: 1,
-      },
-    },
+      expect(response.status).toBe(200)
+      expect(responseBody).toEqual({
+        updated_at: expect.any(String),
+        dependencies: {
+          database: {
+            version: "16.0",
+            max_connections: 100,
+            opened_connections: 1,
+          },
+        },
+      })
+
+      const parsedDate = new Date(responseBody.updated_at).toISOString()
+      expect(responseBody.updated_at).toBe(parsedDate)
+    })
   })
-
-  const parsedDate = new Date(responseBody.updated_at).toISOString()
-  expect(responseBody.updated_at).toBe(parsedDate)
 })
