6bbb7be24f63fa5c589b6021d57b4b40985ad824
verbuscomunicacao <ferreira@verbuscomunicacao.com.br>
2025-09-22 19:32:16 -0300

    feat: git add `runPendingMigrations`
    method to `orchestrator`

---
+++ tests/api/integration/api/v1/users/post.test.js
@@ -0,0 +1,26 @@
+import database from "infra/database"
+import orchestrator from "tests/orchestrator.js"
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices()
+  await orchestrator.clearDatabase()
+  await orchestrator.runPendingMigrations()
+})
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+        values: ["pedigru3", "contato@curso.dev", "abc123"],
+      })
+
+      const users = await database.query("SELECT * FROM users;")
+      console.log(users.rows)
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      })
+      expect(response.status).toBe(201)
+    })
+  })
+})
