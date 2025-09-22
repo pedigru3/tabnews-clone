4ce3d16c5a04f4ef53c5051f3fedf644fd22e81b
verbuscomunicacao <ferreira@verbuscomunicacao.com.br>
2025-09-22 15:47:53 -0300

    refactor: use orchestrator and correct status

--- tests/api/integration/api/v1/migrations/get.test.js
+++ tests/api/integration/api/v1/migrations/get.test.js
@@ -1,21 +1,17 @@
 import orchestrator from "tests/orchestrator.js"
 
-async function clearDatabase() {
-  await orchestrator.clearDatabase()
-}
-
 beforeAll(async () => {
   await orchestrator.waitForAllServices()
-  await clearDatabase()
+  await orchestrator.clearDatabase()
 })
 
 describe("GET /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     test("Retrieving pending migrations", async () => {
       const response = await fetch("http://localhost:3000/api/v1/migrations")
-      const responseBody = await response.json()
+      expect(response.status).toBe(200)
 
-      expect(process.env.POSTGRES_HOST).toBe("localhost")
+      const responseBody = await response.json()
 
       expect(Array.isArray(responseBody)).toBe(true)
       expect(responseBody.length).toBeGreaterThan(0)
--- tests/api/integration/api/v1/migrations/post.test.js
+++ tests/api/integration/api/v1/migrations/post.test.js
@@ -5,7 +5,7 @@ beforeAll(async () => {
   await orchestrator.clearDatabase()
 })
 
-describe("Post /api/v1/migrations", () => {
+describe("POST /api/v1/migrations", () => {
   describe("Anonymous user", () => {
     describe("Running pending migrations", () => {
       test("For the first time", async () => {
@@ -18,13 +18,15 @@ describe("Post /api/v1/migrations", () => {
         expect(response1.status).toBe(201)
 
         const response1Body = await response1.json()
+
+        expect(Array.isArray(response1Body)).toBe(true)
         expect(response1Body.length).toBeGreaterThan(0)
       })
       test("For the second time", async () => {
         const response2 = await fetch(
           "http://localhost:3000/api/v1/migrations",
           {
-            method: "GET",
+            method: "POST",
           },
         )
         expect(response2.status).toBe(200)
